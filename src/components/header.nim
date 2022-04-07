9fdcbe814712726e280f3510261f678e671dd59b
masahirookubo <stmhamachiii@gmail.com>
2022-04-07 23:01:13 +0900

    modified header classes

--- src/components/header.nim
+++ src/components/header.nim
@@ -1,7 +1,12 @@
 include karax/prelude
 
 proc renderHeader*(): VNode =
-  buildHtml():
-    tdiv(class = "navbar bg-neutral text-neutral-content"):
-      a(class="btn btn-ghost normal-case text-xl"):
-        text "hoge"
+  buildHtml(header(class = "text-gray-100 bg-gray-900 body-font shadow w-full")):
+    tdiv(class = "container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"):
+      nav(class = "flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto"):
+        a(class = "mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600"):
+          text "About us"
+        a(class = "mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600"):
+          text "Products"
+        a(class = "mr-5 hover:text-gray-900 cursor-pointer border-b border-transparent hover:border-indigo-600"):
+          text "Investors"
